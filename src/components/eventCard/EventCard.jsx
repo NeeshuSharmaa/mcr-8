3419542fd307ba7ad17267d718c4c28fe711c10d
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-07-13 21:41:02 +0530

    feat:landing page fxnality

--- src/components/eventCard/EventCard.jsx
+++ src/components/eventCard/EventCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./EventCard.css";
 export default function EventCard({
   id,
@@ -7,13 +8,15 @@ export default function EventCard({
   eventType,
 }) {
   return (
-    <div className="event-card">
-      <div className="img-div">
-        <img src={eventThumbnail} alt={title} />
+    <Link to={`/events/${id}`}>
+      <div className="event-card">
+        <div className="img-div">
+          <img src={eventThumbnail} alt={title} />
+        </div>
+        <small>{eventStartTime}</small>
+        <h3>{title}</h3>
+        <span className="event-type-tag">{eventType}</span>
       </div>
-      <small>{eventStartTime}</small>
-      <h3>{title}</h3>
-      <span className="event-type-tag">{eventType}</span>
-    </div>
+    </Link>
   );
 }
